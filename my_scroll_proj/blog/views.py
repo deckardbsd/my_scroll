6eea2c0c329672a8a6600d9913753302b967596a
Sabin Boboc <bobocsabin@gmail.com>
2017-12-09 01:08:15 +0200

    replace post_list function with GCBV IndexView

--- my_scroll_proj/blog/views.py
+++ my_scroll_proj/blog/views.py
@@ -1,23 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views import generic
 
 from .models import Post
 
 # Create your views here.
 
-def post_list(request):
-    posts_list=Post.objects.all()
-    paginator = Paginator(posts_list, 3)
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an int, deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If empty page, get the last one
-        posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+class IndexView(generic.ListView):
+    model = Post
+    template_name = 'blog/post/list.html'
+    context_object_name = 'posts'
+    paginate_by = 3
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
